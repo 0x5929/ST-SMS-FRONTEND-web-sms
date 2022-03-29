b906a5182995f8b35b003d99d3a8d778c0ff2aa9
rennitbaby <work.kevin.ren@gmail.com>
2022-03-29 01:13:59 -0700

    changed the style def, and added some school related fields

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -11,21 +11,33 @@ import RadioGroup from '@mui/material/RadioGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import FormControl from '@mui/material/FormControl'
 import FormLabel from '@mui/material/FormLabel'
+import Grid from '@mui/material/Grid';
 
 
+const UserTextField = styled(TextField)(( {theme} ) => ({
+  'margin' : theme.spacing(1),
+  'width': '80%',
+  'display' : 'block'
+}));
 
-const CustomizedTextField = styled(TextField)`
-    margin-top: 20px;
-    margin-bottom: 20px;
-    display: block;
+const CustomizedFormControl = styled(FormControl)(( {theme} ) => ({
+  'margin' : theme.spacing(1),
+  'display' : 'block'
+}));
 
-`
-const CustomizedFormControl = styled(FormControl)`
-    margin-top: 20px;
-    margin-bottom: 20px;
-    display: block;
+const initialValues = {
+  id: 0,
+  studentID: '',
+  firstName: '',
+  lastName: '',
+  phoneNumber: '',
+  email: '',
+  mailingAddress: '',
+  course: 'CNA',
+  programStart: new Date(),
+  programComp: new Date(),
 
-`
+}
 
 export default function Create() {
     const [title, setTitle] = useState('')
@@ -59,52 +71,123 @@ export default function Create() {
           color="textSecondary"
           component="h2"
           gutterBottom
+          sx={{ margin: 1}}
         >
-          Create a New Note
+          Create a New Student Record
         </Typography>
         
         <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-          <CustomizedTextField
-            onChange={(e) => setTitle(e.target.value)}
-            label="Note Title" 
-            variant="outlined" 
-            color="secondary" 
-            fullWidth
-            required
-            error={titleError}
-          />
-          <CustomizedTextField
-            onChange={(e) => setDetails(e.target.value)}
-            label="Details"
-            variant="outlined"
-            color="secondary"
-            multiline
-            rows={4}
-            fullWidth
-            required
-            error={detailsError}
-          />
-  
-          {/* <Radio value="hello" />
-          <Radio value="goodbye" /> */}
-  
-          <CustomizedFormControl>
-            <FormLabel>Note Category</FormLabel>
-            <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
-              <FormControlLabel value="money" control={<Radio />} label="Money" />
-              <FormControlLabel value="todos" control={<Radio />} label="Todos" />
-              <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
-              <FormControlLabel value="work" control={<Radio />} label="Work" />
-            </RadioGroup>
-          </CustomizedFormControl>
-  
-          <Button
-            type="submit" 
-            color="secondary" 
-            variant="contained"
-            endIcon={<KeyboardArrowRightIcon />}>
-            Submit
-          </Button>
+          <Grid container>
+          {/* This was the original fields*/}
+            {/* <CustomizedTextField
+              onChange={(e) => setTitle(e.target.value)}
+              label="Note Title" 
+              variant="outlined" 
+              color="secondary" 
+              fullWidth
+              required
+              error={titleError}
+            />
+            <CustomizedTextField
+              onChange={(e) => setDetails(e.target.value)}
+              label="Details"
+              variant="outlined"
+              color="secondary"
+              multiline
+              rows={4}
+              fullWidth
+              required
+              error={detailsError}
+            />
+    */}
+            <Grid item xs={6}>
+              <UserTextField
+                label="Student ID" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+              <UserTextField
+                label="First Name" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+              <UserTextField
+                label="Last Name" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+              <UserTextField
+                label="Phone Number" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+              <UserTextField
+                label="Email" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+              <UserTextField
+                label="Mailing Address" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+              <UserTextField
+                label="Course" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+              <UserTextField
+                label="Program Start Date" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+              <UserTextField
+                label="Program Completion Date" 
+                variant="outlined" 
+                color="secondary" 
+                fullWidth
+                required
+              />
+
+
+              {/* <Radio value="hello" />
+              <Radio value="goodbye" /> */}
+      
+              <CustomizedFormControl>
+                <FormLabel>Note Category</FormLabel>
+                <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+                  <FormControlLabel value="money" control={<Radio />} label="Money" />
+                  <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+                  <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
+                  <FormControlLabel value="work" control={<Radio />} label="Work" />
+                </RadioGroup>
+              </CustomizedFormControl>
+      
+              <Button
+                type="submit" 
+                color="secondary" 
+                variant="contained"
+                endIcon={<KeyboardArrowRightIcon />}>
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
         </form>
   
         
