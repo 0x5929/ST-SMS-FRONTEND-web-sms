52e75ca4f4a31b2dadb9dcde1c05907f33899f8c
First Obj Oriented <work.kevin.ren@gmail.com>
2022-05-22 23:49:35 -0700

    updated component file of Statistics
    still need to yarn add recharts, and refactor styling for each component into its styles.js

--- src/features/Query/Statistics/component.jsx
+++ src/features/Query/Statistics/component.jsx
@@ -1,12 +1,71 @@
 import React from "react";
 import Styles from './styles';
 
+import { useTheme } from '@mui/material/styles';
 
-export default function Statistics() {
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Label,
+  ResponsiveContainer
+} from "recharts";
 
+
+export default function Statistics({title, data}) {
+
+
+  const theme = useTheme();
 
     
     return (
-        <div>Statistics</div>
+        <ResponsiveContainer>
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 16,
+                right: 16,
+                bottom: 0,
+                left: 24,
+              }}
+            >
+              <XAxis 
+                dataKey="year"
+                stroke={theme.palette.text.secondary}
+                style={theme.typography.body2}
+              >
+                 <Label
+                    angle={0}
+                    position="bottom"
+                    style={{
+                        textAnchor: 'middle',
+                        fill: theme.palette.text.primary,
+                        ...theme.typography.body1,
+                    }}
+                >
+                    {title}
+            </Label>
+              </XAxis>
+              <YAxis          
+                stroke={theme.palette.text.secondary}
+                style={theme.typography.body2}
+              />
+              <Tooltip />
+              <Line
+                isAnimationActive={false}
+                type="monotone"
+                dataKey="count"
+                stroke={theme.palette.primary.main}
+                dot={false}
+              />
+            </LineChart>
+       </ResponsiveContainer>
     )
 }
+
+
+
