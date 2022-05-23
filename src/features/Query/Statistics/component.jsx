0ef70685a0890d90632c51bcf847c87fad33f2e6
rennitbaby <work.kevin.ren@gmail.com>
2022-05-22 23:21:48 -0700

    added statistics subcomponent folder for the query feature

---
+++ src/features/Query/Statistics/component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from './styles';
+
+
+export default function Statistics() {
+
+
+    
+    return (
+        <div>Statistics</div>
+    )
+}
---
+++ src/features/Query/Statistics/styles.js
@@ -0,0 +1,14 @@
+import { styled } from '@mui/material/styles';
+
+
+// const Grid = styled(MuiGrid)(( {theme} ) => ({
+//     // styling Grid if needed
+// }));
+
+
+
+const Styles = {
+
+}
+
+export default Styles 
