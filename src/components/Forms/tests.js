04dbb9bd4ea6edb39d0e6fd61052a4c5c39047f8
0x5929 <mail.kevin.ren@gmail.com>
2022-07-28 16:51:32 -0700

    started form testing

--- src/components/Forms/tests.js
+++ src/components/Forms/tests.js
@@ -1,1 +1,24 @@
-// tests go here
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import StudentForm from './StudentForm'
+
+describe('testing form components', () => {
+    describe('testing StudentForm component', () => {
+        it('should render form with input components', () => {
+
+        })
+
+        it('should invoke submit when Submit is pressed', () => {
+
+        })
+
+        it('should invoke EditSubmit when Submit is pressed in Edit', () => {
+
+        })
+
+        it('should clear all fields and errors when Cancel is pressed', () => {
+            
+        })
+    })
+})
