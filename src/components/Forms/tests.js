863e87437a7ff06e069f41a393ec45b91c2b512c
rennitbaby <work.kevin.ren@gmail.com>
2022-08-04 09:00:14 -0700

    added the second way to do the testing, see if we can convert the rest, and if it can work

--- src/components/Forms/tests.js
+++ src/components/Forms/tests.js
@@ -6,10 +6,68 @@ import Components from '../../components'
 
 import StudentForm from './StudentForm'
 
-afterEach(cleanup)
 
 describe('testing form components', () => {
+
+
     describe('testing StudentForm component', () => {
+
+
+        function getNotificationResults() {
+            const { result } = renderHook(() => useNotification(Components.NotificationSlide))
+            return result.current
+        }
+
+        let setup;
+
+        beforeEach(() => {
+            setup = () => {
+                const props = {
+                    inputOnChange: jest.fn() 
+                }
+
+
+                const notificationResults = getNotificationResults()
+                const { result } = renderHook( () => useStudentForm({notificationHandlers: notificationResults[0], notify: notificationResults[1]}))
+    
+                
+                render(
+                    <StudentForm 
+                        studentFormStates={result.current[0]}
+                        studentFormHandlers={result.current[1]}
+                    />)
+
+                return {
+
+                    getInput(labelText) {
+                        return screen.getByLabelText(labelText)
+                    },
+                    inputOnChange: props.inputOnChange
+                }
+            }
+        })
+        afterEach(() => {
+            setup = undefined;
+            cleanup();
+        
+        })
+
+
+        test('text inputs', () => {
+            const { getInput } = setup()
+            const inputLabels = [
+                'Student ID',
+                'First Name'
+            ]
+
+            let textInput
+            for( var label in inputLabels ) {
+                textInput = getInput(label)
+                expect(textInput).toBeInTheDocument()
+            }
+
+        })
+
         it('should render form with text input components', () => {
             // call useForm, and pass in necessary values to studentForm.
             // use react-hooks-testing-library
