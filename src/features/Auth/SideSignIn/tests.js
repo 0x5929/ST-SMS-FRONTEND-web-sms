6a05a3c652768b860da562212e5acc8ee9f99dbd
First Obj Oriented <work.kevin.ren@gmail.com>
2022-08-24 09:48:35 -0700

    Update tests.js

--- src/features/Auth/SideSignIn/tests.js
+++ src/features/Auth/SideSignIn/tests.js
@@ -3,7 +3,7 @@ import { render, screen, cleanup } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { BrowserRouter } from 'react-router-dom'
 import { renderHook } from '@testing-library/react-hooks/dom' 
-import { useSignInForm } from '../../../hooks'
+import { useSignInForm } from '../../../hooks' // should we just comment this out? that might work
 
 import { AuthContextProvider, useAuthContext } from '../../../contexts'
 import Signin from './component'
