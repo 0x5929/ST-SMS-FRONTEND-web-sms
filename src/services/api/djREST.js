4d214963315fa6cef60fecf4b848b073473658d1
rennitbaby <work.kevin.ren@gmail.com>
2022-10-09 01:52:58 -0700

    added some pseudocode for the upcoming integration, with authentcation, and roadmap on what else needs to be done

--- src/services/api/djREST.js
+++ src/services/api/djREST.js
@@ -1,14 +1,43 @@
-import axios from 'axios';
-
+import axios from 'axios'
 
 export const serverBaseUrl = 'http://127.0.0.1:8000/'
 
-// GET request for student statistics
-export const studentStatisticsGET = () => {
-    let statisticsApi = 'api/sms/student_statistics/'
+// POST request for authentication
+export const authenticationPOST = async (creds) => {
+    let authUrl = 'auth/login/'
+    let authBody = creds
 
-    axios.get(statisticsApi).then((response) => {
+    try {
+        const response = await axios.post(serverBaseUrl + authUrl, authBody)
         console.log(response.data)
+
         return response.data
-    })
+    }
+    catch(error) {
+        console.error(error)
+    }
 }
+
+// GET request for student statistics
+export const studentStatisticsGET = async () => {
+    let statisticsUrl = 'api/sms/student_statistics/'
+
+    try {
+        const response = await axios.get(serverBaseUrl + statisticsUrl)
+        console.log(response)
+
+        return response.data
+    }
+    catch(error) {
+        console.error(error)
+    }
+}
+
+
+// GET request for student query
+
+// POST request for new student
+
+// PUT request for student edit
+
+// DELETE request for student deletion
