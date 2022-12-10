96c9eeb7fd1c28543b8675368a4133fd6fd73bb0
rennitbaby <work.kevin.ren@gmail.com>
2022-12-10 10:26:03 -0800

    added production settings for project

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -1,9 +1,29 @@
+/**
+ *  this file imports axios
+ *  and helps resolve import issues in djREST
+ *  
+ *  this is also encapsulates axios, 
+ *  so that we can easily edit its configuration settings that the whole project uses
+ * 
+ */
+
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8000/'
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+console.log('process.env.REACT_APP_BACKEND_SERVER_URL: ', process.env.REACT_APP_BACKEND_SERVER_URL)
 
-// withCredentials will send http only auth cookies to server to authenticate on APIs, NOT on page render/component mounts
-// must also protect private routes/pages with refresh on every visit, also it will give us a fresh accessToken to be used for ... API calls
+
+let BASE_URL
+
+if (process.env.NODE_ENV !== 'production') {
+    BASE_URL = process.env.REACT_APP_BACKEND_SERVER_URL
+}
+
+else {
+    BASE_URL = 'http://localhost:8000/'
+}
+
+
 export default axios.create({
     baseURL: BASE_URL,
     headers: { 'Content-Type' :  'application/json; charset=UTF-8' },
