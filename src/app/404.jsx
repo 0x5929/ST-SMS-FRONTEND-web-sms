e2edf30ebfadc1a015da893e87396d5153e88532
rennitbaby <work.kevin.ren@gmail.com>
2022-12-12 16:58:24 -0800

    deleted unessaryt imports and tried a different way to render button

--- src/app/404.jsx
+++ src/app/404.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Box, Button, Container, Typography, Stack } from '@mui/material';
-import Grid from '@mui/material/Grid';
+import { Box, Button, Typography, Stack } from '@mui/material';
 
 export default function PageNotFound({ Link }) {
   return (
@@ -23,9 +22,9 @@ export default function PageNotFound({ Link }) {
             <Typography variant="h6">
               The page you’re looking for doesn’t exist.
             </Typography>
-            <Link to="/">
-                <Button variant="contained">Back Home</Button>
-            </Link>
+               
+            <Button variant="contained"><Link to="/">Back Home</Link></Button>
+            
 
         </Stack>
     </Box>
