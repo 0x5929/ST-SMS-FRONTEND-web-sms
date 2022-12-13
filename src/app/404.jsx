94230dfde25e5a4f377137060ae0bbe36e18f84a
rennitbaby <work.kevin.ren@gmail.com>
2022-12-12 17:13:07 -0800

    ascii art doesnt work, back to image

--- src/app/404.jsx
+++ src/app/404.jsx
@@ -1,34 +1,6 @@
 import React from 'react';
 import { Box, Button, Typography, Stack } from '@mui/material';
 
-const ascii = `
-                                                       
-                                                       
-       444444444       000000000            444444444  
-      4::::::::4     00:::::::::00         4::::::::4  
-     4:::::::::4   00:::::::::::::00      4:::::::::4  
-    4::::44::::4  0:::::::000:::::::0    4::::44::::4  
-   4::::4 4::::4  0::::::0   0::::::0   4::::4 4::::4  
-  4::::4  4::::4  0:::::0     0:::::0  4::::4  4::::4  
- 4::::4   4::::4  0:::::0     0:::::0 4::::4   4::::4  
-4::::444444::::4440:::::0 000 0:::::04::::444444::::444
-4::::::::::::::::40:::::0 000 0:::::04::::::::::::::::4
-4444444444:::::4440:::::0     0:::::04444444444:::::444
-          4::::4  0:::::0     0:::::0          4::::4  
-          4::::4  0::::::0   0::::::0          4::::4  
-          4::::4  0:::::::000:::::::0          4::::4  
-        44::::::44 00:::::::::::::00         44::::::44
-        4::::::::4   00:::::::::00           4::::::::4
-        4444444444     000000000             4444444444
-                                                       
-                                                       
-                                                       
-                                                       
-                                                       
-                                                       
-                                                       
-`
-
 
 export default function PageNotFound({ Link }) {
   return (
@@ -37,14 +9,18 @@ export default function PageNotFound({ Link }) {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        margin: 'auto',
         minHeight: '100vh'
       }}
     >
         <Stack>
 
-            <Typography variant="pre">
-              {ascii}
-            </Typography>
+            <img
+              src="/404.jpg"
+              alt=""
+              width={500} height={250}
+            />
+            
             <Typography variant="h6">
               The page you’re looking for doesn’t exist.
             </Typography>
