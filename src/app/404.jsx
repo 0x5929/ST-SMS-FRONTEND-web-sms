a928d5915db3fb8b42efce5faeea563aaa298ae0
rennitbaby <work.kevin.ren@gmail.com>
2022-12-12 17:09:50 -0800

    changed from image to ascci instead

--- src/app/404.jsx
+++ src/app/404.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import { Box, Button, Typography, Stack } from '@mui/material';
 
+const ascii = `
+                                                       
+                                                       
+       444444444       000000000            444444444  
+      4::::::::4     00:::::::::00         4::::::::4  
+     4:::::::::4   00:::::::::::::00      4:::::::::4  
+    4::::44::::4  0:::::::000:::::::0    4::::44::::4  
+   4::::4 4::::4  0::::::0   0::::::0   4::::4 4::::4  
+  4::::4  4::::4  0:::::0     0:::::0  4::::4  4::::4  
+ 4::::4   4::::4  0:::::0     0:::::0 4::::4   4::::4  
+4::::444444::::4440:::::0 000 0:::::04::::444444::::444
+4::::::::::::::::40:::::0 000 0:::::04::::::::::::::::4
+4444444444:::::4440:::::0     0:::::04444444444:::::444
+          4::::4  0:::::0     0:::::0          4::::4  
+          4::::4  0::::::0   0::::::0          4::::4  
+          4::::4  0:::::::000:::::::0          4::::4  
+        44::::::44 00:::::::::::::00         44::::::44
+        4::::::::4   00:::::::::00           4::::::::4
+        4444444444     000000000             4444444444
+                                                       
+                                                       
+                                                       
+                                                       
+                                                       
+                                                       
+                                                       
+`
+
+
 export default function PageNotFound({ Link }) {
   return (
     <Box
@@ -13,18 +42,15 @@ export default function PageNotFound({ Link }) {
     >
         <Stack>
 
-            <img
-              src="/404.jpg"
-              alt=""
-              width={500} height={250}
-            />
-            
+            <Typography variant="pre">
+              {ascii}
+            </Typography>
             <Typography variant="h6">
               The page you’re looking for doesn’t exist.
             </Typography>
-               
-            <Button variant="contained"><Link to="/">Back Home</Link></Button>
-            
+            <Link to="/">
+                <Button variant="contained">Back Home</Button>
+            </Link>
 
         </Stack>
     </Box>
