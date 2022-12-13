d1c79d153a384e867e5207cb719552475c14704e
rennitbaby <work.kevin.ren@gmail.com>
2022-12-12 16:49:09 -0800

    added 404 image and use stacks isntead

--- src/app/404.jsx
+++ src/app/404.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Typography, Stack } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
 export default function PageNotFound({ Link }) {
@@ -12,28 +12,22 @@ export default function PageNotFound({ Link }) {
         minHeight: '100vh'
       }}
     >
-      <Container maxWidth="md">
-        <Grid container spacing={2}>
-          <Grid mobile={6}>
-            <Typography variant="h1">
-              404
-            </Typography>
+        <Stack>
+
+            <img
+              src="/404.jpg"
+              alt=""
+              width={500} height={250}
+            />
+            
             <Typography variant="h6">
               The page you’re looking for doesn’t exist.
             </Typography>
             <Link to="/">
                 <Button variant="contained">Back Home</Button>
             </Link>
-          </Grid>
-          <Grid mobile={6}>
-            <img
-              src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
-              alt=""
-              width={500} height={250}
-            />
-          </Grid>
-        </Grid>
-      </Container>
+
+        </Stack>
     </Box>
   );
 }
