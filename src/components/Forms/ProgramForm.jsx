16dc77498c59d73b70fe63b4af66f29d78fb5476
rennitbaby <work.kevin.ren@gmail.com>
2022-12-13 19:45:13 -0800

    disabled rotation changing while in edit mode

--- src/components/Forms/ProgramForm.jsx
+++ src/components/Forms/ProgramForm.jsx
@@ -63,12 +63,13 @@ function ProgramForm({ validations, studentFormStates, studentFormHandlers, ...o
                     value={rotation}
                     errorHandler={validations.rotation}
                     handleChange={handleRotationChange}
-                    showError={showError}    
+                    showError={showError} 
+                    disabled={recordForEdit ? true : false}   
 
                     data-testid="rotation-select"                
                 />
                 {
-                    course && 
+                    (course && !recordForEdit) &&  
                     
                     <Styles.AddRotBtn 
                         size="medium" 
